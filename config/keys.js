894bfb03bc26547a58829a2367411a3491ca83da
Anthony Gaff <anthony.gaff@valid.com>
2018-10-14 12:46:58 -0400

    midway through auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://anthony41:anthony41@ds019756.mlab.com:19756/devconnector"
+  mongoURI:
+    "mongodb://anthony41:anthony41@ds019756.mlab.com:19756/devconnector",
+  secretOrKey: "secret"
 };
